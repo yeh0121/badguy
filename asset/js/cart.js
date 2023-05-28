18504f513e8960796a2aaf76815013b372f09f11
yeh0121 <yxuqiao0121@gmail.com>
2023-05-28 09:19:37 +0800

    back

--- asset/js/cart.js
+++ asset/js/cart.js
@@ -58,16 +58,85 @@ function payDropdown() {
       dropdownContent.style.display = "none";
   }
 }
-var button = document.getElementById("PAYmethod");
-button.addEventListener("click", function() {
-      button.style.backgroundColor = "#00ff00"; 
-      button.style.color = "#000000"; 
-    });
-    function showcredit() {
-      var textBox = document.getElementById("text-box");
-      textBox.style.display = "block"; 
+// var button = document.getElementById("PAYmethod");
+// button.addEventListener("click", function() {
+//       button.style.backgroundColor = "#00ff00"; 
+//       button.style.color = "#000000"; 
+//     });
+//     function showcredit() {
+//       var textBox = document.getElementById("text-box");
+//       textBox.style.display = "block"; 
+//     }
+
+
+
+
+var selectedLocation = null; 
+    function LocationColor(LocationId) {
+      var button = document.getElementById(LocationId);
+
+      if (selectedLocation !== null) {
+        selectedLocation.classList.remove('active'); 
+      }
+
+      if (selectedLocation === button) {
+        selectedLocation = null; 
+      } else {
+        button.classList.add('active'); 
+        selectedLocation = button; 
+      }
+      var eleven = document.getElementById("elevenDropdown");
+      if (LocationId === 'location1') {
+        eleven.style.display = "block"; 
+      } else {
+        eleven.style.display = "none"; 
+      }
+      var family = document.getElementById("familyDropdown");
+      if (LocationId === 'location2') {
+        family.style.display = "block"; 
+      } else {
+        family.style.display = "none"; 
+      }
+      var diliver = document.getElementById("diliverDropdown");
+      if (LocationId === 'location3') {
+        diliver.style.display = "block"; 
+      } else {
+        diliver.style.display = "none"; 
+      }
+
     }
 
+var selectedButton = null; 
+function changeColor(buttonId) {
+  var button = document.getElementById(buttonId);
+
+  if (selectedButton !== null) {
+    selectedButton.classList.remove('active'); 
+  }
+
+  if (selectedButton === button) {
+    selectedButton = null; 
+  } else {
+    button.classList.add('active'); 
+    selectedButton = button; 
+  }
+  var textBox = document.getElementById("payDropdown");
+  if (buttonId === 'button2') {
+    textBox.style.display = "block"; 
+  } else {
+    textBox.style.display = "none"; 
+  }
+
+}
+
+function touch() {
+  var box = document.getElementById("text-box");
+  if (box.style.display === "block") {
+    box.style.display = "none";
+  } else {
+    box.style.display = "block";
+  }
+}
       
   
 
